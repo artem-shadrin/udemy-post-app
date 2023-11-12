5cab1beab01314e01e6e5f77bd8722366fdfd0c0
Artem Shadrin <explay97@gmail.com>
2023-11-12 22:39:42 +0500

    gh pages

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./components/app/index.jsx";
-import 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
